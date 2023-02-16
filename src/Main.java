a2cc1357b39db20cfacb57c4e74ad9a4b0e8d4c1
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:52:46 -0500

    fix conflicts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             //FOR INUTIL
             System.out.println(i);
